5a311e78c705d11a1cf932b55d7bb8f09b14bc6e
jhenrique09 <jhsv09@gmail.com>
2023-01-06 23:15:59 -0300

    bramble: Regenerate

--- bramble-vendor.mk
+++ bramble-vendor.mk
@@ -220,6 +220,7 @@ PRODUCT_COPY_FILES += \
     vendor/google/bramble/proprietary/system_ext/etc/permissions/com.verizon.llkagent.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.verizon.llkagent.xml \
     vendor/google/bramble/proprietary/system_ext/etc/permissions/dpmapi.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/dpmapi.xml \
     vendor/google/bramble/proprietary/system_ext/etc/permissions/embms-noship_product_privapp_permissions_qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/embms-noship_product_privapp_permissions_qti.xml \
+    vendor/google/bramble/proprietary/system_ext/etc/permissions/google-ril.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/google-ril.xml \
     vendor/google/bramble/proprietary/system_ext/etc/permissions/org_codeaurora_ims.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/org_codeaurora_ims.xml \
     vendor/google/bramble/proprietary/system_ext/etc/permissions/qcrilhook.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/qcrilhook.xml \
     vendor/google/bramble/proprietary/system_ext/etc/permissions/vzw_mvs_permissions.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/vzw_mvs_permissions.xml \
@@ -618,12 +619,14 @@ PRODUCT_PACKAGES += \
     com.qualcomm.qti.services.secureui \
     datastatusnotification \
     uceShimService \
+    ConnectivityThermalPowerManager \
     EuiccGoogleOverlay \
     EuiccSupportPixel \
     EuiccSupportPixelPermissions \
     HbmSVManager \
     LLKAgent \
     PixelNfc \
+    grilservice \
     ims \
     qcrilmsgtunnel \
     libhwinfo \
@@ -653,6 +656,7 @@ PRODUCT_PACKAGES += \
     com.quicinc.cne.constants-V2.0-java \
     com.quicinc.cne.constants-V2.1-java \
     dpmapi \
+    google-ril \
     qcrilhook \
     qmapbridge \
     vendor.qti.data.factory-V1.0-java \
